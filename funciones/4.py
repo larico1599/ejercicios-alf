65faa27ae550606c475d7c448afee3e86426f948
Jhonatan <larico1599@gmail.com>
2021-03-13 23:20:58 -0500

    ejercicio 4 alf

---
+++ funciones/4.py
@@ -0,0 +1,19 @@
+#Escribir una función que calcule el total de una factura tras aplicarle el IVA.
+#  La función debe recibir la cantidad sin IVA y el porcentaje de IVA a aplicar,
+#  y devolver el total de la factura. Si se invoca la función sin pasarle el porcentaje
+#  de IVA, deberá aplicar un 21%.
+
+def factura (monto,igv):
+       
+    monto = monto + monto*(igv/100)
+    print('El monto es %s con un  igv de %s porciento '%(monto,igv))
+
+#monto,igv = list(input("Ingrese el monto y el igv: ").split())
+monto = int(input("Ingrese la cantidad: "))
+porcentage = input("Ingrese el porcentage: ")
+if porcentage.isdigit():
+    porcentage = int(porcentage)
+else:
+    porcentage = 21
+
+factura(monto,porcentage)
